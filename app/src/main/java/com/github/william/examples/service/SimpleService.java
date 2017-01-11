fcfbb70c6741ba351fff4bb3e94c4dc85e48633d
androidStorm <williamg.dev@gmail.com>
2017-01-10 21:17:02 -0600

    Add Simple Service

---
+++ app/src/main/java/com/github/william/examples/service/SimpleService.java
@@ -0,0 +1,40 @@
+package com.github.william.examples.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by wgutierrez on 1/10/17.
+ */
+
+public class SimpleService extends Service {
+
+    private String TAG = "SimpleService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
